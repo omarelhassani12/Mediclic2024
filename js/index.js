f661f607277eb687c82bb107e1de50f9160616c6
Omar EL HASSANI <94725110+omarelhassani12@users.noreply.github.com>
2024-04-30 00:04:12 +0100

    separate the files of styles and script

--- js/index.js
+++ js/index.js
@@ -29,109 +29,6 @@ function toggleDropdown(contentId, iconId) {
     }
 }
 
-// //for the navigation pages
-// document.addEventListener('DOMContentLoaded', () => {
-//     const prevBtn = document.getElementById('prevBtn');
-//     const nextBtn = document.getElementById('nextBtn');
-//     const pagesContainer = document.querySelector('.pages');
-//     const tableRows = document.querySelectorAll('#tableBody tr');
-//     const rowsPerPage = 10;
-//     let currentPage = 1;
-
-//     function showPage(pageNumber) {
-//         const startIndex = (pageNumber - 1) * rowsPerPage;
-//         const endIndex = pageNumber * rowsPerPage;
-
-//         tableRows.forEach((row, index) => {
-//             if (index >= startIndex && index < endIndex) {
-//                 row.style.display = 'table-row';
-//             } else {
-//                 row.style.display = 'none';
-//             }
-//         });
-//     }
-
-//     function updatePagesNavigation(totalPages) {
-//         pagesContainer.innerHTML = '';
-
-//         const maxDisplayedPages = 5; 
-//         const ellipsisThreshold = 2; 
-//         let startPage = 1;
-//         let endPage = Math.min(maxDisplayedPages, totalPages);
-
-//         if (currentPage > ellipsisThreshold) {
-//             startPage = currentPage - 1;
-//             if (totalPages - startPage > maxDisplayedPages) {
-//                 endPage = startPage + maxDisplayedPages - 1;
-//             } else {
-//                 endPage = totalPages;
-//                 startPage = endPage - maxDisplayedPages + 1;
-//             }
-//         }
-
-//         for (let i = startPage; i <= endPage; i++) {
-//             const pageNumberElement = document.createElement('span');
-//             pageNumberElement.textContent = i;
-//             pageNumberElement.classList.add('page-number');
-//             if (i === currentPage) {
-//                 pageNumberElement.classList.add('current-page'); 
-//             }
-//             pagesContainer.appendChild(pageNumberElement);
-//         }
-
-//         if (startPage > ellipsisThreshold + 1) {
-//             const ellipsisElement = document.createElement('span');
-//             ellipsisElement.textContent = '...';
-//             ellipsisElement.classList.add('ellipsis');
-//             pagesContainer.insertBefore(ellipsisElement, pagesContainer.firstChild);
-//         }
-
-//         if (endPage < totalPages - ellipsisThreshold) {
-//             const ellipsisElement = document.createElement('span');
-//             ellipsisElement.textContent = '...';
-//             ellipsisElement.classList.add('ellipsis');
-//             pagesContainer.appendChild(ellipsisElement);
-//         }
-
-//         if (currentPage === 1) {
-//             prevBtn.disabled = true;
-//             prevBtn.style.color = '#858C95';
-//         } else {
-//             prevBtn.disabled = false;
-//             prevBtn.style.color = '#3D8CEF';
-//         }
-
-//         if (currentPage === totalPages) {
-//             nextBtn.disabled = true;
-//             nextBtn.style.color = '#858C95';
-//         } else {
-//             nextBtn.disabled = false;
-//             nextBtn.style.color = '#3D8CEF';
-//         }
-//     }
-
-//     function goToPage(pageNumber) {
-//         currentPage = pageNumber;
-//         showPage(currentPage);
-//         updatePagesNavigation(Math.ceil(tableRows.length / rowsPerPage)); 
-//     }
-
-//     prevBtn.addEventListener('click', () => {
-//         if (currentPage > 1) {
-//             goToPage(currentPage - 1);
-//         }
-//     });
-
-//     nextBtn.addEventListener('click', () => {
-//         const totalPages = Math.ceil(tableRows.length / rowsPerPage);
-//         if (currentPage < totalPages) {
-//             goToPage(currentPage + 1);
-//         }
-//     });
-
-//     updatePagesNavigation(Math.ceil(tableRows.length / rowsPerPage));
-//     showPage(currentPage);
-// });
 
 document.addEventListener('DOMContentLoaded', () => {
     const prevBtn = document.getElementById('prevBtn');
@@ -153,6 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
+
     function updatePagesNavigation(totalPages) {
         pagesContainer.innerHTML = '';
     
@@ -182,9 +80,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 pageNumberElement.classList.add('current-page');
             }
             pagesContainer.appendChild(pageNumberElement);
+            
+            // Add event listener to the page number element
+            pageNumberElement.addEventListener('click', () => {
+                const pageNumber = parseInt(pageNumberElement.textContent);
+                goToPage(pageNumber);
+            });
         }
-    
-
     
         if (endPage < totalPages) {
             const ellipsisElement2 = document.createElement('span');
@@ -214,6 +116,7 @@ document.addEventListener('DOMContentLoaded', () => {
             nextBtn.style.color = '#3D8CEF';
         }
     }
+    
     
 
     function goToPage(pageNumber) {
