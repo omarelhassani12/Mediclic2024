fde80f407489bff26cc73afb1e8b1f33efba312b
Omar EL HASSANI <94725110+omarelhassani12@users.noreply.github.com>
2024-05-03 20:48:53 +0100

    add some script for the Droits view

--- js/droits.js
+++ js/droits.js
@@ -4,3 +4,25 @@ function toggleAllCheckboxes(checkbox) {
         box.checked = checkbox.checked;
     });
 }
+
+// for the count of the checkboxe toggled
+function updateActiveCounter(sectionId) {
+    const toggleCheckboxes = document.querySelectorAll(`#${sectionId} .toggle-checkbox`);
+    const totalCheckboxes = toggleCheckboxes.length;
+    let activeCount = 0;
+    toggleCheckboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+            activeCount++;
+        }
+    });
+    document.getElementById(`${sectionId}-active-counter`).textContent = `${activeCount}/${totalCheckboxes}`;
+}
+
+document.querySelectorAll('.message-center').forEach(section => {
+    const sectionId = section.id;
+    const toggleCheckboxes = section.querySelectorAll('.toggle-checkbox');
+    toggleCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', () => updateActiveCounter(sectionId));
+    });
+    updateActiveCounter(sectionId);
+});
