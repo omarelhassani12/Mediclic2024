83d2b6c527278d0ca1073530a8a549359cbcc0ed
Omar EL HASSANI <94725110+omarelhassani12@users.noreply.github.com>
2024-05-24 21:40:24 +0100

    editing the side bar and add responsive design to it to be compatible with all table types

--- js/side-bar-toggles.js
+++ js/side-bar-toggles.js
@@ -1,14 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('toggleBtn').addEventListener('click', function() {
-        const sidebar = document.querySelector('.sidebar');
-        const mainContent = document.querySelector('.main-content');
-
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM fully loaded and parsed");
+    
+    const toggleBtn = document.querySelector('.toggle-button');
+    const sidebar = document.querySelector('.sidebar');
+    const mainContent = document.querySelector('.main-content');
+    const toggleImgLeft = document.querySelector('.toggle-img-left');
+    const toggleImgRight = document.querySelector('.toggle-img-right');
+    
+    toggleBtn.addEventListener('click', function() {
         if (sidebar.classList.contains('small')) {
             sidebar.classList.remove('small');
             mainContent.classList.remove('small');
+            toggleImgLeft.classList.remove('hidden');
+            toggleImgRight.classList.add('hidden');
         } else {
             sidebar.classList.add('small');
             mainContent.classList.add('small');
+            toggleImgLeft.classList.add('hidden');
+            toggleImgRight.classList.remove('hidden');
         }
     });
 });
+
