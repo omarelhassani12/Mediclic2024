e6cb55c0721c3af52237c89ebc6044e461e777b0
Omar EL HASSANI <94725110+omarelhassani12@users.noreply.github.com>
2024-06-19 13:32:02 +0100

    fix not find search btn in index.js

--- js/index.js
+++ js/index.js
@@ -1,34 +1,183 @@
 
 
+// document.addEventListener('DOMContentLoaded', () => {
+//     const searchBtn = document.getElementById('searchBtn');
+//     searchBtn.addEventListener('click', () => {
+//         const startDateValue = document.getElementById('startDate').value;
+//         const endDateValue = document.getElementById('endDate').value;
+//         const statusValue = document.getElementById('status').value;
+
+//         console.log('Start Date:', startDateValue);
+//         console.log('End Date:', endDateValue);
+//         console.log('Status:', statusValue);
+//     });
+// });
+
+
+// function toggleDropdown(contentId, iconId) {
+//     var dropdownContent = document.getElementById(contentId);
+//     var dropdownIcon = document.getElementById(iconId);
+
+//     dropdownContent.classList.toggle("active");
+
+//     if (dropdownContent.classList.contains("active")) {
+//         dropdownContent.style.display = "block";
+//         dropdownIcon.classList.add("rotate-icon");
+//     } else {
+//         dropdownContent.style.display = "none";
+//         dropdownIcon.classList.remove("rotate-icon");
+//     }
+// }
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const prevBtn = document.getElementById('prevBtn');
+//     const nextBtn = document.getElementById('nextBtn');
+//     const pagesContainer = document.querySelector('.pages');
+//     const tableRows = document.querySelectorAll('#tableBody tr');
+//     const rowsPerPage = 10;
+//     let currentPage = 1;
+
+//     function showPage(pageNumber) {
+//         const startIndex = (pageNumber - 1) * rowsPerPage;
+//         const endIndex = pageNumber * rowsPerPage;
+
+//         tableRows.forEach((row, index) => {
+//             if (index >= startIndex && index < endIndex) {
+//                 row.style.display = 'table-row';
+//             } else {
+//                 row.style.display = 'none';
+//             }
+//         });
+//     }
+
+//     function updatePagesNavigation(totalPages) {
+//         pagesContainer.innerHTML = '';
+    
+//         const maxDisplayedPages = 5;
+//         const ellipsisThreshold = 2;
+//         let startPage = 1;
+//         let endPage = totalPages;
+    
+//         if (totalPages > maxDisplayedPages) {
+//             endPage = Math.min(maxDisplayedPages - 1, totalPages); // Displaying first 4 pages
+//             if (currentPage > ellipsisThreshold) {
+//                 startPage = currentPage - ellipsisThreshold;
+//                 if (totalPages - startPage > maxDisplayedPages - 2) {
+//                     endPage = startPage + maxDisplayedPages - 2;
+//                 } else {
+//                     endPage = totalPages;
+//                     startPage = endPage - maxDisplayedPages + 3;
+//                 }
+//             }
+//         }
+    
+//         for (let i = startPage; i <= endPage; i++) {
+//             const pageNumberElement = document.createElement('span');
+//             pageNumberElement.textContent = i;
+//             pageNumberElement.classList.add('page-number');
+//             if (i === currentPage) {
+//                 pageNumberElement.classList.add('current-page');
+//             }
+//             pagesContainer.appendChild(pageNumberElement);
+            
+//             // Add event listener to the page number element
+//             pageNumberElement.addEventListener('click', () => {
+//                 const pageNumber = parseInt(pageNumberElement.textContent);
+//                 goToPage(pageNumber);
+//             });
+//         }
+    
+//         if (endPage < totalPages) {
+//             const ellipsisElement2 = document.createElement('span');
+//             ellipsisElement2.textContent = '...';
+//             ellipsisElement2.classList.add('ellipsis');
+//             pagesContainer.appendChild(ellipsisElement2);
+    
+//             const lastPageElement = document.createElement('span');
+//             lastPageElement.textContent = totalPages;
+//             lastPageElement.classList.add('page-number');
+//             pagesContainer.appendChild(lastPageElement);
+//         }
+    
+//         if (currentPage === 1) {
+//             prevBtn.disabled = true;
+//             prevBtn.style.color = '#858C95';
+//         } else {
+//             prevBtn.disabled = false;
+//             prevBtn.style.color = '#3D8CEF';
+//         }
+    
+//         if (currentPage === totalPages) {
+//             nextBtn.disabled = true;
+//             nextBtn.style.color = '#858C95';
+//         } else {
+//             nextBtn.disabled = false;
+//             nextBtn.style.color = '#3D8CEF';
+//         }
+//     }
+    
+    
+
+//     function goToPage(pageNumber) {
+//         currentPage = pageNumber;
+//         showPage(currentPage);
+//         updatePagesNavigation(Math.ceil(tableRows.length / rowsPerPage));
+//     }
+
+//     prevBtn.addEventListener('click', () => {
+//         if (currentPage > 1) {
+//             goToPage(currentPage - 1);
+//         }
+//     });
+
+//     nextBtn.addEventListener('click', () => {
+//         const totalPages = Math.ceil(tableRows.length / rowsPerPage);
+//         if (currentPage < totalPages) {
+//             goToPage(currentPage + 1);
+//         }
+//     });
+
+//     updatePagesNavigation(Math.ceil(tableRows.length / rowsPerPage));
+//     showPage(currentPage);
+// });
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const searchBtn = document.getElementById('searchBtn');
-    searchBtn.addEventListener('click', () => {
-        const startDateValue = document.getElementById('startDate').value;
-        const endDateValue = document.getElementById('endDate').value;
-        const statusValue = document.getElementById('status').value;
+    if (searchBtn) {
+        searchBtn.addEventListener('click', () => {
+            const startDateValue = document.getElementById('startDate').value;
+            const endDateValue = document.getElementById('endDate').value;
+            const statusValue = document.getElementById('status').value;
 
-        console.log('Start Date:', startDateValue);
-        console.log('End Date:', endDateValue);
-        console.log('Status:', statusValue);
-    });
+            console.log('Start Date:', startDateValue);
+            console.log('End Date:', endDateValue);
+            console.log('Status:', statusValue);
+        });
+    } else {
+        console.log('searchBtn element not found');
+    }
 });
-
 
 function toggleDropdown(contentId, iconId) {
     var dropdownContent = document.getElementById(contentId);
     var dropdownIcon = document.getElementById(iconId);
 
-    dropdownContent.classList.toggle("active");
+    if (dropdownContent && dropdownIcon) {
+        dropdownContent.classList.toggle("active");
 
-    if (dropdownContent.classList.contains("active")) {
-        dropdownContent.style.display = "block";
-        dropdownIcon.classList.add("rotate-icon");
+        if (dropdownContent.classList.contains("active")) {
+            dropdownContent.style.display = "block";
+            dropdownIcon.classList.add("rotate-icon");
+        } else {
+            dropdownContent.style.display = "none";
+            dropdownIcon.classList.remove("rotate-icon");
+        }
     } else {
-        dropdownContent.style.display = "none";
-        dropdownIcon.classList.remove("rotate-icon");
+        console.log('One or more elements not found');
     }
 }
-
 
 document.addEventListener('DOMContentLoaded', () => {
     const prevBtn = document.getElementById('prevBtn');
@@ -38,106 +187,108 @@ document.addEventListener('DOMContentLoaded', () => {
     const rowsPerPage = 10;
     let currentPage = 1;
 
-    function showPage(pageNumber) {
-        const startIndex = (pageNumber - 1) * rowsPerPage;
-        const endIndex = pageNumber * rowsPerPage;
+    if (prevBtn && nextBtn && pagesContainer && tableRows) {
+        function showPage(pageNumber) {
+            const startIndex = (pageNumber - 1) * rowsPerPage;
+            const endIndex = pageNumber * rowsPerPage;
 
-        tableRows.forEach((row, index) => {
-            if (index >= startIndex && index < endIndex) {
-                row.style.display = 'table-row';
-            } else {
-                row.style.display = 'none';
-            }
-        });
-    }
-
-    function updatePagesNavigation(totalPages) {
-        pagesContainer.innerHTML = '';
-    
-        const maxDisplayedPages = 5;
-        const ellipsisThreshold = 2;
-        let startPage = 1;
-        let endPage = totalPages;
-    
-        if (totalPages > maxDisplayedPages) {
-            endPage = Math.min(maxDisplayedPages - 1, totalPages); // Displaying first 4 pages
-            if (currentPage > ellipsisThreshold) {
-                startPage = currentPage - ellipsisThreshold;
-                if (totalPages - startPage > maxDisplayedPages - 2) {
-                    endPage = startPage + maxDisplayedPages - 2;
+            tableRows.forEach((row, index) => {
+                if (index >= startIndex && index < endIndex) {
+                    row.style.display = 'table-row';
                 } else {
-                    endPage = totalPages;
-                    startPage = endPage - maxDisplayedPages + 3;
+                    row.style.display = 'none';
                 }
-            }
-        }
-    
-        for (let i = startPage; i <= endPage; i++) {
-            const pageNumberElement = document.createElement('span');
-            pageNumberElement.textContent = i;
-            pageNumberElement.classList.add('page-number');
-            if (i === currentPage) {
-                pageNumberElement.classList.add('current-page');
-            }
-            pagesContainer.appendChild(pageNumberElement);
-            
-            // Add event listener to the page number element
-            pageNumberElement.addEventListener('click', () => {
-                const pageNumber = parseInt(pageNumberElement.textContent);
-                goToPage(pageNumber);
             });
         }
-    
-        if (endPage < totalPages) {
-            const ellipsisElement2 = document.createElement('span');
-            ellipsisElement2.textContent = '...';
-            ellipsisElement2.classList.add('ellipsis');
-            pagesContainer.appendChild(ellipsisElement2);
-    
-            const lastPageElement = document.createElement('span');
-            lastPageElement.textContent = totalPages;
-            lastPageElement.classList.add('page-number');
-            pagesContainer.appendChild(lastPageElement);
-        }
-    
-        if (currentPage === 1) {
-            prevBtn.disabled = true;
-            prevBtn.style.color = '#858C95';
-        } else {
-            prevBtn.disabled = false;
-            prevBtn.style.color = '#3D8CEF';
-        }
-    
-        if (currentPage === totalPages) {
-            nextBtn.disabled = true;
-            nextBtn.style.color = '#858C95';
-        } else {
-            nextBtn.disabled = false;
-            nextBtn.style.color = '#3D8CEF';
-        }
-    }
-    
-    
 
-    function goToPage(pageNumber) {
-        currentPage = pageNumber;
-        showPage(currentPage);
+        function updatePagesNavigation(totalPages) {
+            pagesContainer.innerHTML = '';
+
+            const maxDisplayedPages = 5;
+            const ellipsisThreshold = 2;
+            let startPage = 1;
+            let endPage = totalPages;
+
+            if (totalPages > maxDisplayedPages) {
+                endPage = Math.min(maxDisplayedPages - 1, totalPages); // Displaying first 4 pages
+                if (currentPage > ellipsisThreshold) {
+                    startPage = currentPage - ellipsisThreshold;
+                    if (totalPages - startPage > maxDisplayedPages - 2) {
+                        endPage = startPage + maxDisplayedPages - 2;
+                    } else {
+                        endPage = totalPages;
+                        startPage = endPage - maxDisplayedPages + 3;
+                    }
+                }
+            }
+
+            for (let i = startPage; i <= endPage; i++) {
+                const pageNumberElement = document.createElement('span');
+                pageNumberElement.textContent = i;
+                pageNumberElement.classList.add('page-number');
+                if (i === currentPage) {
+                    pageNumberElement.classList.add('current-page');
+                }
+                pagesContainer.appendChild(pageNumberElement);
+
+                // Add event listener to the page number element
+                pageNumberElement.addEventListener('click', () => {
+                    const pageNumber = parseInt(pageNumberElement.textContent);
+                    goToPage(pageNumber);
+                });
+            }
+
+            if (endPage < totalPages) {
+                const ellipsisElement2 = document.createElement('span');
+                ellipsisElement2.textContent = '...';
+                ellipsisElement2.classList.add('ellipsis');
+                pagesContainer.appendChild(ellipsisElement2);
+
+                const lastPageElement = document.createElement('span');
+                lastPageElement.textContent = totalPages;
+                lastPageElement.classList.add('page-number');
+                pagesContainer.appendChild(lastPageElement);
+            }
+
+            if (currentPage === 1) {
+                prevBtn.disabled = true;
+                prevBtn.style.color = '#858C95';
+            } else {
+                prevBtn.disabled = false;
+                prevBtn.style.color = '#3D8CEF';
+            }
+
+            if (currentPage === totalPages) {
+                nextBtn.disabled = true;
+                nextBtn.style.color = '#858C95';
+            } else {
+                nextBtn.disabled = false;
+                nextBtn.style.color = '#3D8CEF';
+            }
+        }
+
+        function goToPage(pageNumber) {
+            currentPage = pageNumber;
+            showPage(currentPage);
+            updatePagesNavigation(Math.ceil(tableRows.length / rowsPerPage));
+        }
+
+        prevBtn.addEventListener('click', () => {
+            if (currentPage > 1) {
+                goToPage(currentPage - 1);
+            }
+        });
+
+        nextBtn.addEventListener('click', () => {
+            const totalPages = Math.ceil(tableRows.length / rowsPerPage);
+            if (currentPage < totalPages) {
+                goToPage(currentPage + 1);
+            }
+        });
+
         updatePagesNavigation(Math.ceil(tableRows.length / rowsPerPage));
+        showPage(currentPage);
+    } else {
+        console.log('One or more elements not found');
     }
-
-    prevBtn.addEventListener('click', () => {
-        if (currentPage > 1) {
-            goToPage(currentPage - 1);
-        }
-    });
-
-    nextBtn.addEventListener('click', () => {
-        const totalPages = Math.ceil(tableRows.length / rowsPerPage);
-        if (currentPage < totalPages) {
-            goToPage(currentPage + 1);
-        }
-    });
-
-    updatePagesNavigation(Math.ceil(tableRows.length / rowsPerPage));
-    showPage(currentPage);
 });
--- js/side-bar-toggles.js
+++ js/side-bar-toggles.js
@@ -1,24 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOM fully loaded and parsed");
-    
     const toggleBtn = document.querySelector('.toggle-button');
     const sidebar = document.querySelector('.sidebar');
     const mainContent = document.querySelector('.main-content');
     const toggleImgLeft = document.querySelector('.toggle-img-left');
     const toggleImgRight = document.querySelector('.toggle-img-right');
-    
-    toggleBtn.addEventListener('click', function() {
-        if (sidebar.classList.contains('small')) {
-            sidebar.classList.remove('small');
-            mainContent.classList.remove('small');
-            toggleImgLeft.classList.remove('hidden');
-            toggleImgRight.classList.add('hidden');
-        } else {
-            sidebar.classList.add('small');
-            mainContent.classList.add('small');
-            toggleImgLeft.classList.add('hidden');
-            toggleImgRight.classList.remove('hidden');
-        }
-    });
-});
 
+    if (toggleBtn && sidebar && mainContent && toggleImgLeft && toggleImgRight) {
+        toggleBtn.addEventListener('click', function() {
+            if (sidebar.classList.contains('small')) {
+                sidebar.classList.remove('small');
+                mainContent.classList.remove('small');
+                toggleImgLeft.classList.remove('hidden');
+                toggleImgRight.classList.add('hidden');
+            } else {
+                sidebar.classList.add('small');
+                mainContent.classList.add('small');
+                toggleImgLeft.classList.add('hidden');
+                toggleImgRight.classList.remove('hidden');
+            }
+        });
+    } else {
+        console.log("One or more elements not found in the DOM.");
+    }
+});
